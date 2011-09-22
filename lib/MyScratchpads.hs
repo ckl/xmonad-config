6640397713cb29cc3fef451d45a5fe7284447f75
Chris Lu <chris.lu06@gmail.com>
2011-09-22 06:28:44 -0500

    First commit, comitting all configuration files, scripts, and README

---
+++ lib/MyScratchpads.hs
@@ -0,0 +1,210 @@
+---------------------------------------------------------------------
+-- |
+-- Module     : MyScratchpads.hs
+--
+-- This module is similar to XMonad.Util.NamedScratchpad and also
+-- draws inspiration from pbrisbin's ScratchPadKeys. Since Gnome-terminal
+-- seems incompatible with runInTerminal (does not support '-name'),
+-- xterm is used by default. Other additions are more manage hooks
+-- for specifying location, which take width and height parameters,
+-- instead of just a height.
+---------------------------------------------------------------------
+module MyScratchpads 
+    ( Scratchpad
+    , makeScratchpad
+    , makeTerminalScratchpad
+    , getScratchpadKeys
+    , getScratchpadHooks
+      -- Manage hooks
+    , centerScreen
+    , bottomLeft
+    , bottomCenter
+    , bottomRight
+    , bottomEdge
+    , topLeft
+    , topCenter
+    , topRight
+    ) where
+
+import XMonad
+import XMonad.Actions.DynamicWorkspaces (addHiddenWorkspace)
+import XMonad.Hooks.ManageHelpers       (doRectFloat)
+import XMonad.Util.Run
+import XMonad.Util.Scratchpad
+
+import Control.Arrow                    ((&&&))
+import Control.Monad                    (filterM, unless)
+
+import qualified XMonad.StackSet as W
+import qualified Data.Map as M
+
+-- $usage
+-- In your xmonad.hs:
+-- 
+-- > import MyScratchpads
+--
+-- Then create a list of your scratchpads:
+--
+-- > myScratchpads :: [Scratchpad]
+-- > myScratchpads =  [cmus, htop]
+--
+-- Now define what each of your scratchpads should be. A definition
+-- includes whether it's a terminal app or not, the key to bind it to,
+-- the command to run, and the manage hook (where it appears on screen).
+-- Examples:
+--
+-- > cmus :: Scratchpad
+-- > cmus = makeTerminalScratchpad (mod4Mask, xK_m) "cmus" $ centerScreen 0.5 0.5
+--
+-- Here's how to make scratchpads for windowed applications
+-- (Make a scratchpad for lxappearance):
+-- > lxappearance:: Scratchpad
+-- > lxappearance = makeScratchpad (mod4Mask, xK_b)      -- keybinding
+-- >                               "lxappearance"        -- command name
+-- >                               "Lxappearance" $      -- class name (from xprop)
+-- >                               centerScreen 0.5 0.8  -- location hook
+--
+-- Then combine them with your manage hook:
+--
+-- > myManageHook = manageHook defaultConfig <+>
+--                  ... <+> getScratchpadHooks myScratchpads
+--
+-- And don't forget to bind your keys:
+--
+-- > myKeys = M.fromList $
+-- >          [ ... 
+-- >            ...
+-- >          ] ++ getScratchpadKeys myScratchpads
+--
+-- Check out my xmonad.hs for more information.
+
+
+-- | Scratchpad data type.
+data Scratchpad = Scratchpad 
+    { key   :: (KeyMask, KeySym)    -- keybinding to be used
+    , cmd   :: X ()                 -- command to run
+    , query :: Query Bool           -- query to find it once it's spawned
+    , hook  :: ManageHook           -- location of the window on screen
+    }
+
+-- Default terminal used to execute the command. My default terminal,
+-- gnome-terminal, doesn't work (doesn't support '-name' switch), so 
+-- use xterm with these defaults.
+term :: String
+term = "xterm -bg black -fg white -fa dejavu -fs 10 "
+
+-- | mkTermScratchPad : Creates the terminal scratchpad with the specified
+--   keybinding and command to be run. Query is "sp-<command>", and hook is
+--   one of the window location hooks defined at the bottom.
+makeTerminalScratchpad :: (KeyMask, KeySym) -> String -> ManageHook -> Scratchpad
+makeTerminalScratchpad k c h = Scratchpad
+    { key   = k
+    , cmd   = runInTerminal ["-name", "sp-" ++ c, "-e", c]
+    , query = resource =? ("sp-" ++ c)
+    , hook  = h
+    }
+
+-- | makeScratchpad : Creates a scratchpad for the specified window 
+--   application. Similar to makeTerminalScratchpad above, except you
+--   MUST provide the class name to query for the application. You can
+--   find this by typing 'xprop | grep WM_CLASS' and clicking on the 
+--   target application. It's usually the second parameter in the list.
+makeScratchpad :: (KeyMask, KeySym) -> String -> String -> ManageHook -> Scratchpad
+makeScratchpad k c q h = Scratchpad
+    { key   = k
+    , cmd   = spawn c
+    , query = className =? q
+    , hook  = h
+    }
+
+-- | runInTerminal : takes arguments of the command in the form of
+--   a list and returns an X window object, namely, the spawned terminal.
+runInTerminal :: [String] -> X ()
+runInTerminal args = spawn $ term ++ (unwords args) 
+
+-- | getScratchpadKeys : Takes a list of terminal scratchpads as input
+--   (defined above) and returns a list of tuples of keybindings that
+--   are to be appended to the keybindings used in xmonad.hs.
+getScratchpadKeys :: [Scratchpad] -> [((KeyMask, KeySym), X ())]
+getScratchpadKeys = fmap (key &&& spawnScratchpad)
+
+-- | getScratchpadHooks : Takes a list of scratchpads and returns a
+--   manage hook that combines all of them. Combine the returned result
+--   with your manage hook in your xmonad.hs.
+getScratchpadHooks :: [Scratchpad] -> ManageHook
+getScratchpadHooks = composeAll . fmap (\c -> query c --> hook c)
+
+-- | spawnScratchpad : Summon, banish, or spawn a single scratchpad.
+--   (Borrowed and adapted from XMonad.Util.NamedScratchpad.hs)
+spawnScratchpad :: Scratchpad -> X ()
+spawnScratchpad sp = withWindowSet $ \s -> do
+    -- Search for the window on the current workspace
+    filterCurrent <- filterM (runQuery $ query sp) .
+        maybe [] W.integrate . W.stack . W.workspace $ W.current s
+
+    case filterCurrent of
+        -- The window was not found on the current workspace
+        [] -> do
+            -- Search all workspaces for the window
+            filterAll <- filterM (runQuery $ query sp) $ W.allWindows s
+            -- If it's not found, then run the command. If it is found
+            -- then move the window to the current workspace
+            case filterAll of
+                []    -> cmd sp
+                (x:_) -> windows $ W.shiftWin (W.currentTag s) x
+        -- The window was found on the current workspace
+        (x:_) -> do
+            -- Add the hidden workspace "NSP" if it doesn't already exist
+            unless
+                (any ((== "NSP") . W.tag) $ W.workspaces s) $
+                addHiddenWorkspace "NSP"
+            -- Move the window to the hidden workspace
+            windows $ W.shiftWin "NSP" x
+
+
+--------------------------------------------------------------------------
+-- ManageHooks
+--
+-- Each of these functions takes a width and/or a height, then returns
+-- a manage hook for the specified screen position. 0.99 is used
+-- instead of 1.0 for determining position to leave a small gap
+-- between the border of the screen and the window.
+--
+-- Note: parameters for W.RationalRect are l t w h where
+--       l = distance from the left as a percent
+--       t = distance from the top as a percent
+--       w = width as a percent
+--       h = height as a percent
+
+-- | Floating rectangular window at the bottom left
+bottomLeft :: Rational -> Rational -> ManageHook
+bottomLeft w h = doRectFloat $ W.RationalRect 0.01 (0.99 - h) w h
+
+-- | Floats window along the bottom edge and centers it
+bottomCenter :: Rational -> Rational -> ManageHook
+bottomCenter w h = doRectFloat $ W.RationalRect ((1 - w)/2) (1 - h) w h
+
+-- | Floating rectangular window at the bottom right
+bottomRight :: Rational -> Rational -> ManageHook
+bottomRight w h = doRectFloat $ W.RationalRect (0.99 - w) (0.99 - h) w h
+
+-- | Floating rectangular window at the top left 
+topLeft :: Rational -> Rational -> ManageHook
+topLeft w h = doRectFloat $ W.RationalRect 0.01 0.01 w h
+
+-- | Floats a rectangular window at the top center
+topCenter :: Rational -> Rational -> ManageHook
+topCenter w h = doRectFloat $ W.RationalRect ((1 - w)/2) 0.01 w h
+
+-- | Floating rectangular window at the top right
+topRight :: Rational -> Rational -> ManageHook
+topRight w h = doRectFloat $ W.RationalRect (0.99 - w) 0.01 w h
+
+-- | Floating rectangular window at the center of the screen
+centerScreen :: Rational -> Rational -> ManageHook
+centerScreen w h = doRectFloat $ W.RationalRect ((1 - w)/2) ((1 - h)/2) w h
+
+-- | Floats along the entire bottom edge of the screen with the specified height
+bottomEdge :: Rational -> ManageHook
+bottomEdge h = doRectFloat $ W.RationalRect 0 (1 - h) 1 h
+
---
+++ xmonad.hs
@@ -0,0 +1,328 @@
+-- My XMonad configuration file
+import XMonad
+import XMonad.Actions.CopyWindow
+import XMonad.Actions.WindowGo	
+import XMonad.Config.Desktop
+import XMonad.Hooks.DynamicLog
+import XMonad.Hooks.ManageDocks
+import XMonad.Hooks.ManageHelpers
+import XMonad.Hooks.UrgencyHook
+import XMonad.Layout.IM
+import XMonad.Layout.Maximize
+import XMonad.Layout.Named
+import XMonad.Layout.NoBorders
+import XMonad.Layout.PerWorkspace
+import XMonad.Layout.Reflect
+import XMonad.Layout.ResizableTile
+import XMonad.Layout.Spacing
+import XMonad.Prompt
+import XMonad.Prompt.Shell
+import XMonad.Prompt.Window
+import XMonad.Util.EZConfig(additionalKeysP)
+import XMonad.Util.Run
+import XMonad.Util.Scratchpad
+
+import qualified Data.Map as M
+import qualified XMonad.Layout.Magnifier as Mag
+import qualified XMonad.StackSet as W
+import System.IO
+
+import MyScratchpads
+
+-- Default terminal. Called with mod+shift+enter
+myTerminal = "gnome-terminal"
+
+-- Border width around [in]active windows
+myBorderWidth = 2
+
+-- Border colors for focused and unfocused windows
+myNormalBorderColor    = "#cccccc"
+myFocusedBorderColor   = "#00a2ff"
+
+-- Default mod mask, mod1Mask = alt
+myModMask = mod1Mask
+
+-- Whether or not focus follows the mouse pointer
+myFocusFollowsMouse :: Bool
+myFocusFollowsMouse = True
+
+-- List of workspace names
+myWorkspaces = 
+	[ "1:web"
+	, "2:code"
+	, "3:code"
+	, "4:"
+	, "5:"
+	, "6:"
+	, "7:gimp"
+	, "8:aard"
+	, "9:bt"
+	]
+
+
+-- Default XPConfig color theme used by shellPrompt and windowPromptGoto
+myXPConfig :: XPConfig
+myXPConfig = defaultXPConfig
+    { font = "-*-terminus-*-r-normal-*-*-120-*-*-*-*-iso8859-*"
+    , bgColor = "black"
+    , fgColor = "#59bbe8"
+    , fgHLight = "black"
+    , bgHLight = "#59bbe8"
+    , borderColor = "#59bbe8" }
+
+-----------------------------------------------------------
+-- Programs to run at startup
+-----------------------------------------------------------
+myStartup :: X ()
+myStartup = do
+	spawn "nitrogen --restore"
+    -- only run transmission if it's not already running
+	runOrRaise "transmission" (className =? "transmission")
+
+-----------------------------------------------------------
+-- Window and scratchpad hooks
+-----------------------------------------------------------
+-- 
+myWindowHook = (composeAll . concat $
+    [ [(role =? "gimp-toolbox" <||> role =? "gimp-image-window" <||> role =? "gimp-dock") --> (ask >>= doF . W.sink)]
+    , [ className =? x --> doF (W.shift "7:gimp")   | x <- myGimpApp ]
+    , [ className =? x --> doF (W.shift "8:aard")   | x <- myAardApp ]
+    , [ className =? x --> doF (W.shift "9:bt")     | x <- myBtApp ]
+    , [ className =? x --> doCenterFloat            | x <- myCenterFloats ]
+    , [ isFullscreen   --> doFullFloat]
+    ]) 
+    where
+        role           = stringProperty "WM_WINDOW_ROLE" 
+        myGimpApp      = ["Gimp", "Gimp-2.6"]
+        myAardApp      = ["Wine"]
+        myBtApp        = ["Transmission"]
+        myCenterFloats = ["File-roller"]
+
+-- scratchpad terminal hook called when pressing F12
+terminalScratchpad :: ManageHook
+terminalScratchpad = scratchpadManageHook (W.RationalRect l t w h)
+    where
+        h = 0.50    -- height percent
+        w = 0.50    -- width  percent
+        t = 0.01    -- distance from top  (percent)
+        l = 0.01    -- distance from left (percent)
+
+-- create scratchpads using definitions from MyScratchpad.hs
+myScratchpads :: [Scratchpad]
+myScratchpads =  [cmus, htop]
+
+cmus :: Scratchpad
+cmus = makeTerminalScratchpad (mod4Mask, xK_m) "cmus" $ centerScreen 0.5 0.5
+
+htop :: Scratchpad
+htop = makeTerminalScratchpad (mod4Mask, xK_h) "htop" $ bottomCenter 0.7 0.4
+
+-- combine all of our hooks and call it 'myManageHook'
+myManageHook = manageHook defaultConfig <+>
+               manageDocks              <+>
+               myWindowHook             <+> 
+               terminalScratchpad       <+> getScratchpadHooks myScratchpads
+
+-----------------------------------------------------------
+-- Layouts
+-----------------------------------------------------------
+myLayout = onWorkspace "7:gimp" (named "Gimp" gimpFirst)   $ 
+           named "myTall"       (modifiers tiled)          |||
+           named "myFull"       (noBorders Full)           -- |||
+           -- I find myself almost never using this layout
+           --named "myMirror"     (modifiers $ Mirror tiled) 
+    where 
+        gimpFirst   = gimpLayout ||| modifiers tiled ||| modifiers (Mirror tiled)
+        tiled       = ResizableTall nmaster delta ratio []
+        modifiers x = spacing (7) $ maximize $ avoidStruts $ Mag.magnifiercz 1.0 x
+        -- gimp toolbar is on the left, docks on the right, image in middle
+        gimpLayout  = avoidStruts  $ withIM (0.15) (Role "gimp-toolbox") $
+                      reflectHoriz $ withIM (0.2)  (Role "gimp-dock") 
+                      (ResizableTall 3 delta ratio [])
+        nmaster     = 1
+        delta       = 3/100
+        ratio       = 1/2
+
+-----------------------------------------------------------
+-- logHooks and status bars
+-----------------------------------------------------------
+
+-- Top left status bar and top right conky bar
+myDzenBar  = "dzen2 -h 18 -w 700 -x 5 -ta l " ++ myBarFont
+myConkyBar = "conky -c ~/.xmonad/conkyrc | dzen2 -h 18 -w 980 -x 690 -ta r" ++ myBarFont
+myBarFont  = " -fn '-*-terminus-*-r-normal-*-*-120-*-*-*-*-iso8859-*'"
+
+-- Colors used for left status bar
+myFontColor = "#ffffff"
+
+-- Format is dzenColor <foreground> <background>
+myPPCurrentColor = dzenColor "#1a1a1a" "#00a2ff"
+myPPVisibleColor = dzenColor "#59bbe8" "gray30"
+myPPHiddenColor  = dzenColor myFontColor ""
+myPPTitleColor   = dzenColor myFontColor ""
+myPPLayoutColor  = dzenColor "#59bbe8" ""
+
+myIconPath   = "/home/chris/.xmonad/images/icons/"
+myMarkerIcon = "^i(" ++ myIconPath ++ "marker.xbm)"
+
+myDzenPP h = dzenPP
+    {
+          ppCurrent = myPPCurrentColor . wrap ("["++myMarkerIcon) "]" 
+        , ppVisible = myPPVisibleColor . wrap ("["++myMarkerIcon) "]"
+        , ppHidden  = myPPHiddenColor  . wrap "[" "]" . noScratchpad
+        , ppTitle   = myPPTitleColor   . pad . shorten 50
+        , ppWsSep   = " "
+        , ppSep     = " | "
+        , ppOutput  = hPutStrLn h
+        , ppLayout  = myPPLayoutColor . 
+            (\x -> case x of
+                "myTall"   -> "Tall"        ++ insertLayoutIcon "tall"
+                "myMirror" -> "Mirror Tall" ++ insertLayoutIcon "mtall"
+                "myFull"   -> "Full"        ++ insertLayoutIcon "full"
+                _ -> x
+            )
+    }
+    where
+        insertLayoutIcon :: String -> String
+        insertLayoutIcon icon = " " ++ (getLayoutIcon icon)
+        getLayoutIcon    :: String -> String
+        getLayoutIcon icon = "^i(" ++ myIconPath ++ "layout-" ++ icon ++ ".xbm)"
+        noScratchpad ws = if ws == "NSP" then "" else ws
+
+
+myUrgencyHook = withUrgencyHook dzenUrgencyHook
+    { args = ["-bg", "darkgreeen"] } 
+
+-----------------------------------------------------------
+-- Key bindings
+-----------------------------------------------------------
+myKeys = M.fromList $
+        [ 
+          -----------------------------------------------------------
+          -- XMonad UI keys
+          -----------------------------------------------------------
+          -- toggle panel visibility (doesn't work properly with dzen2)
+          --((myModMask, xK_b), sendMessage ToggleStruts)
+          -- kill conky and dzen2 to hide. restart xmonad to show again
+          ((myModMask, xK_b),  spawn "killall conky && killall dzen2")
+          -- ctrl+q closes active window
+        , ((controlMask, xK_q), kill)                 
+          -- expand tiled window vertically (for ResizableTall layout)
+        , ((myModMask, xK_o), sendMessage MirrorExpand)
+          -- shrink tiled window vertically (for ResizableTall layout)
+	    , ((myModMask, xK_u), sendMessage MirrorShrink) 
+          -- pops open a 'go to window' prompt
+	    , ((mod4Mask, xK_g), windowPromptGoto myXPConfig) 
+          -- increase window magnification
+        , ((myModMask, xK_equal), sendMessage Mag.MagnifyMore) 
+          -- decrease window magnification
+        , ((myModMask, xK_minus), sendMessage Mag.MagnifyLess)
+          -- toggle active window magnifier on/off (grave is `)
+        , ((myModMask, xK_bracketleft), sendMessage Mag.Toggle)
+          -- toggle maximize window
+        , ((myModMask, xK_grave), withFocused (sendMessage . maximizeRestore))
+          -- open xmonad shell to launch applications
+        , ((myModMask, xK_p), shellPrompt myXPConfig)
+          -- open the scratchpad terminal
+        , ((0, xK_F12), terminalScratchpadKey)
+          -----------------------------------------------------------
+          -- Program hotkeys
+          -----------------------------------------------------------
+        , ((mod4Mask, xK_a), spawn "gnome-terminal")
+        , ((mod4Mask, xK_f), spawn "thunar")
+        , ((mod4Mask, xK_x), spawn "chromium-browser")
+        , ((mod4Mask, xK_0), spawn "chromium-browser --incognito")
+        , ((mod4Mask, xK_t), spawn "transmission")
+        , ((mod4Mask, xK_c), spawn "pcmanfm")
+          -----------------------------------------------------------
+	      -- multimedia hotkeys
+          -----------------------------------------------------------
+        , ((mod1Mask,  xK_z),    spawn "cmus-remote --pause")
+        , ((mod1Mask,  xK_a),    spawn "cmus-remote --prev")
+        , ((mod1Mask,  xK_s),    spawn "cmus-remote --next")
+        , ((mod1Mask,  xK_d),    spawn "cmus-remote --stop")
+        , ((myModMask, xK_Up),   spawn "amixer set Master 1+")
+        , ((myModMask, xK_Down), spawn "amixer set Master 1-")
+          -----------------------------------------------------------
+          -- folder hotkeys
+          -----------------------------------------------------------
+        , ((controlMask .|. shiftMask, xK_r), spawn "thunar /")
+        , ((controlMask .|. shiftMask, xK_d), spawn "thunar /media/media/bt")
+        , ((controlMask .|. shiftMask, xK_a), spawn "thunar /media/media/Anime")
+        , ((controlMask .|. shiftMask, xK_z), spawn "thunar /media/media/Music")
+        , ((controlMask .|. shiftMask, xK_x), spawn "thunar /media/media/Movies")
+        , ((controlMask .|. shiftMask, xK_s), spawn "thunar /media/media/tvshows")
+        , ((controlMask .|. shiftMask, xK_c), spawn "thunar /media/documents/documents")
+        , ((controlMask .|. shiftMask, xK_e), spawn "thunar /media/documents/documents/ebooks")
+          -----------------------------------------------------------
+	      -- miscellaneous hotkeys
+          -----------------------------------------------------------
+          -- myModMask+shift+l locks the screen
+        , ((myModMask.|. shiftMask, xK_l), spawn "xscreensaver-command -lock")
+          -- alt+print-screen takes screenshot of next window to be clicked
+        , ((mod1Mask, xK_Print), spawn "sleep 0.2; scrot -s")
+          -- printscreen takes a screenshot of the entire screen
+        , ((0, xK_Print), spawn "scrot")
+          -----------------------------------------------------------
+        ]  ++ getScratchpadKeys myScratchpads
+        -- must set these flags manually to use gnome-terminal with scratchpad
+        where 
+            terminalScratchpadKey = scratchpadSpawnActionCustom 
+                                    "gnome-terminal --disable-factory --name scratchpad"
+
+-----------------------------------------------------------
+-- Mouse bindings
+-----------------------------------------------------------
+
+myMouseBindings :: XConfig Layout -> M.Map (KeyMask, Button) (Window -> X())
+myMouseBindings (XConfig {XMonad.modMask = modMask}) = M.fromList $
+    [ 
+      -- mod-button1 %! Set the window to floating mode and move by dragging
+      ((modMask, button1), (\w -> focus w >> mouseMoveWindow w
+                                          >> windows W.shiftMaster))
+      -- mod-button2 %! Raise the window to the top of the stack
+    , ((modMask, button2), (\w -> focus w >> windows W.shiftMaster))
+      -- mod-button3 %! Set the window to floating mode and resize by dragging
+    , ((modMask, button3), (\w -> focus w >> mouseResizeWindow w
+                                            >> windows W.shiftMaster))
+      -- hold control + alt and click on a window to swap it with the master
+    , ((controlMask .|. mod1Mask, button1), (\w -> focus w >> windows W.swapMaster))
+    ]
+
+-----------------------------------------------------------
+
+main = do
+    xmproc <- spawnPipe myDzenBar
+    conky  <- spawnPipe myConkyBar
+    xmonad $ myUrgencyHook $ defaultConfig
+        { 
+          -- basic settings
+          focusFollowsMouse  = myFocusFollowsMouse
+	    , workspaces         = myWorkspaces
+	    , borderWidth        = myBorderWidth
+        , terminal           = myTerminal
+        , modMask            = myModMask
+        , normalBorderColor  = myNormalBorderColor
+        , focusedBorderColor = myFocusedBorderColor
+
+          -- layouts and hooks 
+        , startupHook = myStartup
+        , layoutHook  = myLayout
+        , logHook     = dynamicLogWithPP $ myDzenPP xmproc 
+--        , manageHook  = myManageHook <+> manageHook defaultConfig
+        , manageHook = myManageHook
+
+          -- key/mouse bindings
+        , keys          = \c -> myKeys `M.union` keys defaultConfig c
+        , mouseBindings = myMouseBindings
+        } 
+
+
+--------------------------
+-- Old loghook:
+--------------------------
+-- xmproc <- spawnPipe "xmobar"
+-- logHook = dynamicLogWithPP $ xmobarPP
+-- { ppOutput = hPutStrLn xmproc
+-- , ppTitle = xmobarColor "green" "" . shorten 50
+-- }
