1395a37c35ea48cccc208f5e549cc38af011a822
Chris Lu <chris.lu06@gmail.com>
2012-05-06 15:33:20 -0500

    added install.sh, modified readme

---
+++ install.sh
@@ -0,0 +1,24 @@
+# copy config files
+cp ./xmonad.hs ~/.xmonad/
+cp ./conkyrc ~/.xmonad/
+cp -r ./lib/ ~/.xmonad/
+
+# copy bin files
+mkdir ~/bin
+cp -r ./bin/* ~/bin
+echo "make sure ~/bin is in \$PATH"
+
+# icons
+mkdir -p ~/.xmonad/images/icons
+tar -zxvf ./images.tar.gz
+mv ./images/icons/* ~/.xmonad/images/icons/
+rmdir ./images/icons
+rmdir ./images
+tar -zxvf ./thayer-icons.tar.gz
+mv ./thayer/* ~/.xmonad/images/icons/
+rmdir ./thayer
+tar -zxvf ./xbm8x8-icons.tar.gz
+mv ./xbm8x8/* ~/.xmonad/images/icons/
+rmdir ./xbm8x8
+
+echo "done.\nreload your xmonad config"
